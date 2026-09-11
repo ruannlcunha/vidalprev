1606db379d766ca17d373641eed45b90b7049b49
Ruann <ruannlopes2017@gmail.com>
2026-09-11 00:57:48 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vidalprev/sw.js', { scope: '/vidalprev/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
